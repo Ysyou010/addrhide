83852c01b3fa887ea30d45b1f6c5a68c13816ecc
Ysyou010 <131797487+Ysyou010@users.noreply.github.com>
2026-06-16 11:16:07 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='addrhide',
-    version='1.0.0',
-    description='M3U API Key Hiding Proxy Plugin',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'requests',
-        'flask'
-    ],
-)
+# 메인 서버 구동 시 setup()이 자동 실행되는 것을 방지합니다.
+if __name__ == '__main__':
+    setup(
+        name='addrhide',
+        version='1.0.0',
+        description='M3U API Key Hiding Proxy Plugin',
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=[
+            'requests',
+            'flask'
+        ],
+    )
