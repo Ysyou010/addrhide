35ecc8ce2e4b4b05badc6e91e8e2d7b9ff71e2b7
Ysyou010 <131797487+Ysyou010@users.noreply.github.com>
2026-06-16 11:27:31 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
-from .setup import P
-from .mod_main import ModuleMain
+try:
+    from .setup import P
+    from .mod_main import ModuleMain
 
-# 시스템이 시작될 때 메인 모듈을 불러옵니다.
-P.set_module_list([ModuleMain])
+    P.set_module_list([ModuleMain])
+except Exception as e:
+    import traceback
+    print(f"[addrhide] Init Error: {e}")
+    print(traceback.format_exc())
