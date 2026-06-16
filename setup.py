daa8edf11bf078cca40e65e3cd3a93df8a6d76a1
Ysyou010 <131797487+Ysyou010@users.noreply.github.com>
2026-06-16 11:23:36 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-from setuptools import setup, find_packages
+from plugin import PluginManager
 
-# 메인 서버 구동 시 setup()이 자동 실행되는 것을 방지합니다.
-if __name__ == '__main__':
-    setup(
-        name='addrhide',
-        version='1.0.0',
-        description='M3U API Key Hiding Proxy Plugin',
-        packages=find_packages(),
-        include_package_data=True,
-        install_requires=[
-            'requests',
-            'flask'
-        ],
-    )
+# 프레임워크에 addrhide 패키지를 등록합니다.
+P = PluginManager('addrhide', __file__)
