e1cb1cadec02fec5529907f15222e2cbadee0656
Ysyou010 <131797487+Ysyou010@users.noreply.github.com>
2026-06-16 11:22:10 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,4 @@
-from flask import Blueprint
+from plugin import PluginManager
 
-# 'addrhide'라는 이름으로 Blueprint(라우팅 그룹) 생성
-blueprint = Blueprint('addrhide', __name__, template_folder='templates')
-
-# logic.py의 내용을 불러와 라우팅 적용
-from . import logic
+# 프레임워크에 addrhide 패키지를 등록합니다.
+P = PluginManager('addrhide', __file__)
