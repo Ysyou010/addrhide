c6afac56f92d77443251ec2e092ebbcec2db6495
Ysyou010 <131797487+Ysyou010@users.noreply.github.com>
2026-06-16 11:31:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
 from plugin import *
 
-P = PluginManager('addrhide', __file__)
+# FlaskFarm 프레임워크 표준 플러그인 설정 규격
+setting = {
+    'filepath' : __file__,
+    'use_db': True,
+    'use_default_setting': True,
+    'home_module': None,
+    'menu': {
+        'uri': __package__,
+        'name': '주소 숨김 프록시',
+        'list': [
+            {
+                'uri': 'main/setting',
+                'name': '설정',
+            }
+        ]
+    },
+    'setting_menu': None,
+    'default_route': 'normal',
+}
+
+# 프레임워크에 플러그인 객체(P) 생성 및 등록
+P = create_plugin_instance(setting)
